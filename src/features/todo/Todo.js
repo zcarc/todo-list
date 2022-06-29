ebff595f27277730b69262ad90cda3bee53cf528
Hyunsoo Lee <wrkbrs@gmail.com>
2022-06-29 21:09:11 +0900

    add Todo

--- src/features/todo/Todo.js
+++ src/features/todo/Todo.js
@@ -1,8 +1,15 @@
 import React from "react";
+import TodoInput from "../../components/TodoInput";
+import TodoList from "../../components/TodoList";
 import TodoTemplate from "../../components/TodoTemplate";
 
 function Todo() {
-  return <TodoTemplate />;
+  return (
+    <TodoTemplate>
+      <TodoInput />
+      <TodoList />
+    </TodoTemplate>
+  );
 }
 
 export default Todo;
